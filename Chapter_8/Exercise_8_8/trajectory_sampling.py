d796704d705b8cdc5576387d9cb6ee33427a268a
carlfredriksson <c@msp.se>
2023-03-14 00:56:21 +0100

    Worked on exercise 8.8

--- Chapter_8/Exercise_8_8/trajectory_sampling.py
+++ Chapter_8/Exercise_8_8/trajectory_sampling.py
@@ -1,3 +1,17 @@
 import numpy as np
-import random
 import matplotlib.pyplot as plt
+
+class Environment:
+    def __init__(self, num_states, num_actions, b):
+        self.transitions = []
+        for _ in range(num_states):
+            t = []
+            for _ in range(num_actions):
+                t.append(self._init_transitions(num_states, b))
+            self.transitions.append(t)
+
+    def _init_transitions(self, num_states, b):
+        next_states = np.random.randint(0, num_states, b)
+        # The last expected reward is for the transition to the terminal state
+        expected_rewards = np.random.normal(size=b+1)
+        return next_states, expected_rewards

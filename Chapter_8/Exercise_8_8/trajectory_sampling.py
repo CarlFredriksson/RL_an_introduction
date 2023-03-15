8d2b6a16ceb64a533586f0b3d84995fbc7536a58
carlfredriksson <c@msp.se>
2023-03-15 01:09:29 +0100

    Worked on exercise 8.8

--- Chapter_8/Exercise_8_8/trajectory_sampling.py
+++ Chapter_8/Exercise_8_8/trajectory_sampling.py
@@ -35,3 +35,26 @@ class Environment:
         next_state = next_states[rand_index]
 
         return expected_reward, next_state, next_state == self.terminal_state
+
+def arg_max_random_tie_break(x):
+     return np.random.choice(np.flatnonzero(x == x.max()))
+
+def max_random_tie_break(x):
+    return x[arg_max_random_tie_break(x)]
+
+def update_Q(Q, transitions, state, action):
+    next_states, expected_rewards = transitions[state][action]
+    b = len(next_states)
+    update = 0
+    for i in range(b):
+        transition_probability = (1 - 0.1) / b
+        update += transition_probability * (expected_rewards[i] + max_random_tie_break(Q[next_states[i]]))
+    update += 0.1 * expected_rewards[-1]
+
+def eps_greedy_action_selection(Q, state, eps):
+    # Select a random action with probability eps
+    if np.random.random() < eps:
+        return np.random.choice(np.arange(len(Q[state])))
+    
+    # Select the maximizing action - ties broken randomly
+    return arg_max_random_tie_break(Q[state])
